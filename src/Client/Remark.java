3e345931e00f51b30574c1f60ad4ab8a43d557b1
yudai kichise <kichisecreative@gmail.com>
2017-06-14 22:03:50 +0900

    Remarkクラスを記述
    フィールドの設定の仕方を検討する必要がある。
    Remarkクラス内で各単語を用意するか、Conversationクラス内で用意するか

--- src/Client/Remark.java
+++ src/Client/Remark.java
@@ -1,7 +1,33 @@
 package Client;
 
+import java.util.HashMap;
+
 /**
  * 相手の発言の最小単位を表すクラス
+ * 発言文字列と、返答内容による得点を格納するHashMapを用意する。
+ * HashMapのkeyは返答を表す定数とし、valueはその得点とする。
+ * key値が存在するかを事前に、containsKey(Object key)で確認し、存在しない場合にはデフォルト値を返すような実装も可能
+ * フィールドの値をどこで設定するかが問題(RemarkかConversationか)
  */
 public class Remark {
+    private String word;
+    private HashMap<Integer, Integer> score;
+
+    public Remark() {
+
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public int getScore(int replyNum) {
+        if (score.containsKey(replyNum)) {
+            // key値が存在した場合
+            return score.get(replyNum);
+        } else {
+            // key値が存在しない場合、デフォルト値を返す
+            return 0;
+        }
+    }
 }
