60bb34e52e416e05b612302825125a00b95d95f8
yudai kichise <kichisecreative@gmail.com>
2017-06-14 21:47:39 +0900

    Userクラスの作成

---
+++ src/Client/User.java
@@ -0,0 +1,38 @@
+package Client;
+
+/**
+ * ユーザー情報を格納するクラス
+ * サーバーで生成, クライアントで管理する
+ */
+
+import java.util.*;
+
+
+public class User {
+    private String name;
+    private ArrayList<Boolean> clearedStege;
+    private ArrayList<Integer> highScore;
+
+    User(String name) {
+        this.name = name;
+    }
+
+    public void setClearedStage(ArrayList<Boolean> argList) {
+        // ArrayList<T>のコンストラクタにArrayList<T>のインスタンスを渡すとディープコピーされる。
+        // クリア状況をコピー
+        clearedStege = new ArrayList<Boolean>(argList);
+    }
+
+    public void setHighScore(ArrayList<Integer> argList) {
+        // ハイスコア情報をコピー
+        highScore = new ArrayList<Integer>(argList);
+    }
+
+    public ArrayList<Boolean> getClearedStege() {
+        return clearedStege;
+    }
+
+    public ArrayList<Integer> getHighScore() {
+        return highScore;
+    }
+}
