0c074c62e0673861975917ad782b25a8df12d520
yudai kichise <kichisecreative@gmail.com>
2017-06-30 00:52:19 +0900

    Conversationクラスをテストするクラスを作成

---
+++ src/Test/ConversationTest.java
@@ -0,0 +1,29 @@
+package Test;
+
+import Client.Conversation;
+
+import java.util.Timer;
+
+/**
+ * Created by yudaikichise on 2017/06/30.
+ */
+public class ConversationTest {
+    public static void main(String[] args) {
+        
+        Conversation conv;
+        for (int convNum = 1; convNum <= 10; convNum++) {
+            System.out.println("convNum: " + convNum);
+            conv = new Conversation(convNum);
+            while (!conv.getIsEnd()) {
+                System.out.println("getWord: " + conv.getRemark() );
+                System.out.println("scores");
+                for (int replyNum = 1; replyNum <= 12; replyNum++) {
+                    System.out.print(" [" + replyNum + "]:" + conv.getScore(replyNum));
+                }
+                System.out.println();
+                System.out.println();
+            }
+        }
+
+    }
+}
