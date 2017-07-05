d10a9571002aac4e98f1e118e8f47bd040415d24
nyannkostudent <kichisecreative@gmail.com>
2017-07-05 22:24:08 +0900

    StartPanelに画像追加

--- src/Client/StartPanel.java
+++ src/Client/StartPanel.java
@@ -15,11 +15,11 @@ public class StartPanel extends JLayeredPane {
     private JLabel backgroundLabel;
     private JLabel titleLabel;
 
-    private final int buttonWidth = 160;
-    private final int buttonHeight =  90;
+    private final int buttonWidth = 230;
+    private final int buttonHeight =  75;
 
-    private final int titleWidth = 320;
-    private final int titleHeight = 180;
+//    private final int titleWidth = 320;
+//    private final int titleHeight = 180;
 
 
 
@@ -29,7 +29,9 @@ public class StartPanel extends JLayeredPane {
         setLayout(null);
 
         // ログインボタン
-        buttonToLogin = new JButton("ログイン");
+        buttonToLogin = new JButton();
+        buttonToLogin.setIcon(new ImageIcon("./resource/image/buttons/loginBtn.png"));
+        buttonToLogin.setBorderPainted(false);
         buttonToLogin.setBounds(Constants.VIEW_WIDTH * 1 / 3 - buttonWidth / 2, Constants.VIEW_HEIGHT * 2 / 3, buttonWidth, buttonHeight);
         add(buttonToLogin);
         setLayer(buttonToLogin, JLayeredPane.PALETTE_LAYER, 10);
@@ -39,7 +41,9 @@ public class StartPanel extends JLayeredPane {
         });
 
         // 新規ユーザーボタン
-        buttonToNew = new JButton("はじめての人");
+        buttonToNew = new JButton();
+        buttonToNew.setIcon(new ImageIcon("./resource/image/buttons/signUpBtn.png"));
+        buttonToNew.setBorderPainted(false);
         buttonToNew.setBounds(Constants.VIEW_WIDTH * 2 / 3 - buttonWidth / 2, Constants.VIEW_HEIGHT * 2 / 3, buttonWidth, buttonHeight);
         add(buttonToNew);
         setLayer(buttonToNew, JLayeredPane.PALETTE_LAYER, 10);
@@ -48,22 +52,22 @@ public class StartPanel extends JLayeredPane {
             refToAppView.switchStartPanelToNewUserPanel();
         });
 
-        // タイトルラベル
-        titleLabel = new JLabel("タイトル");
-        titleLabel.setBounds(Constants.VIEW_WIDTH / 2 - titleWidth / 2, Constants.VIEW_HEIGHT * 1 / 3 - titleHeight / 2, titleWidth, titleHeight);
-        titleLabel.setOpaque(true);
-        titleLabel.setBackground(Color.magenta);
-        titleLabel.setHorizontalAlignment(JLabel.CENTER);
-        add(titleLabel);
-        setLayer(titleLabel, JLayeredPane.DEFAULT_LAYER, 10);
+//        // タイトルラベル
+//        titleLabel = new JLabel("タイトル");
+//        titleLabel.setBounds(Constants.VIEW_WIDTH / 2 - titleWidth / 2, Constants.VIEW_HEIGHT * 1 / 3 - titleHeight / 2, titleWidth, titleHeight);
+//        titleLabel.setOpaque(true);
+//        titleLabel.setBackground(Color.magenta);
+//        titleLabel.setHorizontalAlignment(JLabel.CENTER);
+//        add(titleLabel);
+//        setLayer(titleLabel, JLayeredPane.DEFAULT_LAYER, 10);
 
 
         // 背景ラベル
         backgroundLabel = new JLabel();
-        backgroundLabel.setBackground(Color.cyan);
+//        backgroundLabel.setBackground(Color.cyan);
         backgroundLabel.setBounds(0, 0, Constants.VIEW_WIDTH, Constants.VIEW_HEIGHT);
-        backgroundLabel.setOpaque(true);
-        backgroundLabel.setIcon(new ImageIcon("../resource/image"));
+//        backgroundLabel.setOpaque(true);
+        backgroundLabel.setIcon(new ImageIcon("./resource/image/background/start.png"));
         add(backgroundLabel);
         setLayer(backgroundLabel, JLayeredPane.DEFAULT_LAYER, 100);
 
